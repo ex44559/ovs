f2c331b2b6a49c6a1674819d2bfa8bf031d50418
ex44559 <sunbo.90s@gmail.com>
2018-05-15 10:05:19 +0800

    fixbug

--- lib/ovs-dm.c
+++ lib/ovs-dm.c
@@ -1,18 +1,36 @@
 #include <config.h>
+#include <stdio.h>
+#include <stdbool.h>
 #include "ovs-dm.h"
 #include "openvswitch/vlog.h"
-#include <stdbool.h>
+#include "util.h"
+
 
 
 VLOG_DEFINE_THIS_MODULE(ovs_dm);
 
-char *errorMsg;
+char errorMsg[200];
 
 void ovs_dm_set_alb_mode(void) {
-	
+	char *cmd = xasprintf("ovs-vsctl set port bond0 bond_mode=balance-aslb");
+	FILE *pp = popen(cmd, "r");
+
+	char buffer[200];
+	if (fgets(buffer, sizeof(buffer), pp) != NULL) {
+		strcpy(errorMsg, buffer);
+	}
+	return;
 }
 
 void ovs_dm_set_none_alb_mode(void) {
+	char *cmd = xasprintf("ovs-vsctl set port bond0 bond_mode=balance-slb");
+	FILE *pp = popen(cmd, "r");
+
+	char buffer[200];
+	if (fgets(buffer, sizeof(buffer), pp) != NULL) {
+		strcpy(errorMsg, buffer);
+	}
+	return;
 
 }
 
@@ -22,6 +40,5 @@ bool ovs_dm_process_to_node(int processToNode) {
 }
 
 char *ovs_dm_get_error_message(void) {
-	char *err = "mealloc error";
-	return err;
+	return errorMsg;
 }
